51f20630f962aa0292abd3b02e8fcabe1b169ee8
Debian <florian@micsi.3lxit5p245levjxr1zmgvzbbia.parx.internal.cloudapp.net>
2023-03-09 15:45:33 +0000

    to end

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('You are the best man and Nasser too'))
+app.get('/', (req, res) => res.send('And now .... nothing !'))
 
 app.all('*', (req, res) => res.status(500).send('You are so bad man'))
 
