4b4d5ad70198ce47e6cacd4b1d823c1804652a5d
Debian <florian@micsi.3lxit5p245levjxr1zmgvzbbia.parx.internal.cloudapp.net>
2023-03-09 13:03:00 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('You are the best man'))
+
+app.all('*', (req, res) => res.status(500).send('You are so bad man'))
+
+app.listen(8888, () => {
+    console.log("Server started on port 8888")
+})
